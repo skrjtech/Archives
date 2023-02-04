e62d2efd2d2b780c40d3e36d3816c3df06663abe
skrjtech <skrjtech@skrtech.com>
2023-02-05 06:13:05 +0900

    pycharm installer script

---
+++ installer/pycharm.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sudo apt update && sudo apt -y upgrade
+# java
+sudo apt install -y default-jre
+sudo apt install -y default-jdk
+
+PYCHARM_VERSION=$1
+# pycahrm
+sudo apt-get install -y curl
+curl -OL https://download.jetbrains.com/python/pycharm-community-$PYCHARM_VERSION.tar.gz
+tar xzf pycharm-community-$PYCHARM_VERSION.tar.gz -C /tmp/
+rm pycharm-community-$PYCHARM_VERSION.tar.gz
+mv /tmp/pycharm-community-$PYCHARM_VERSION /tmp/pycharm
+ln -s /tmp/pycharm/bin/pycharm.sh /tmp/pycharm/bin/pycharm
+export PATH=/tmp/pycharm/bin:$PATH
+echo "export PATH=${PATH}" >> ~/.bashrc
