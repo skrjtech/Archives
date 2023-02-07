8579483b108c590886d9c26c09ea04d2d0dd6065
skrjtech <skrjtech@skrtech.com>
2023-02-08 02:45:55 +0900

    vscode devcontainer start up

--- dockertprojects/composition/vscodedevcontainer/startupdev.sh
+++ dockertprojects/composition/vscodedevcontainer/startupdev.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-bash <(curl https://github.com/skrjtech/archives/tree/main/dockertprojects/composition/vscodedevcontainer/.devcontainer)
-bash <(curl https://github.com/skrjtech/archives/tree/main/dockertprojects/composition/vscodedevcontainer/.docker)
+bash <(wget -r https://github.com/skrjtech/archives/tree/main/dockertprojects/composition/vscodedevcontainer/.devcontainer)
+bash <(wget -r https://github.com/skrjtech/archives/tree/main/dockertprojects/composition/vscodedevcontainer/.docker)
