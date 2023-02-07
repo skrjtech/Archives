f80b6f000b3d1e3cf7afebef217bd647577d19ca
skrjtech <skrjtech@skrtech.com>
2023-02-08 02:50:42 +0900

    vscode devcontainer start up

--- dockertprojects/composition/vscodedevcontainer/startupdev.sh
+++ dockertprojects/composition/vscodedevcontainer/startupdev.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-bash <(wget -r https://github.com/skrjtech/archives/tree/main/dockertprojects/composition/vscodedevcontainer/.devcontainer)
-bash <(wget -r https://github.com/skrjtech/archives/tree/main/dockertprojects/composition/vscodedevcontainer/.docker)
+
+current=$PWD
+
+mkdir .devcontainer
+mkdir .docker
+
+cd .devcontainer
+bash <(curl https://raw.githubusercontent.com/skrjtech/archives/main/dockertprojects/composition/vscodedevcontainer/.devcontainer/devcontainer.json)
+cd ..
+cd .docker
+bash <(curl https://raw.githubusercontent.com/skrjtech/archives/main/dockertprojects/composition/vscodedevcontainer/.docker/Dockerfile)
+bash <(curl https://raw.githubusercontent.com/skrjtech/archives/main/dockertprojects/composition/vscodedevcontainer/.docker/docker-compose.yml)
+
+cd $current
