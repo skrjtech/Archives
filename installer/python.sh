4ef7e3b97675c47366cbdf98b0d8a78a0840bb5b
skrjtech <skrjtech@skrtech.com>
2023-03-26 20:40:56 +0900

    記述の更新

--- installer/python.sh
+++ installer/python.sh
@@ -3,8 +3,10 @@ export DEBIAN_FRONTEND=noninteractive
 # PYTHON INSTALL
 PYTHON_VERSION=$1
 sudo -E apt install -y tk-dev 
-sudo -E apt install -y curl make cmake xz-utils uuid-dev libdb-dev libssl-dev zlib1g-dev libbz2-dev libffi-dev \
-                    libgdbm-dev liblzma-dev libsqlite3-dev build-essential libreadline-dev libncursesw5-dev
+sudo -E apt install -y curl make cmake 
+sudo -E apt install -y xz-utils uuid-dev libdb-dev libssl-dev zlib1g-dev libbz2-dev libffi-dev
+sudo -E apt install -y libgdbm-dev liblzma-dev libsqlite3-dev build-essential libreadline-dev libncursesw5-dev
+
 curl -O https://www.python.org/ftp/python/${PYTHON_VERSION}/Python-${PYTHON_VERSION}.tar.xz      
 tar xJf Python-$PYTHON_VERSION.tar.xz                                                        
 rm -rf Python-$PYTHON_VERSION.tar.xz                                                         
