2e673d7beed8ad7c4ebc723c0d84ac25c711da15
skrjtech <skrjtech@skrtech.com>
2024-07-02 23:24:42 +0900

    universal sudo

--- Installer/python.sh
+++ Installer/python.sh
@@ -4,10 +4,25 @@ export DEBIAN_FRONTEND=noninteractive
 
 # PYTHON INSTALL
 PYTHON_VERSION=$1
-sudo -E apt install -y tk-dev 
-sudo -E apt install -y curl make cmake 
-sudo -E apt install -y xz-utils uuid-dev libdb-dev libssl-dev zlib1g-dev libbz2-dev libffi-dev
-sudo -E apt install -y libgdbm-dev liblzma-dev libsqlite3-dev build-essential libreadline-dev libncursesw5-dev
+
+# sudo -E apt install -y tk-dev 
+# sudo -E apt install -y curl make cmake 
+# sudo -E apt install -y xz-utils uuid-dev libdb-dev libssl-dev zlib1g-dev libbz2-dev libffi-dev
+# sudo -E apt install -y libgdbm-dev liblzma-dev libsqlite3-dev build-essential libreadline-dev libncursesw5-dev
+
+# パッケージのインストールコマンド
+install_cmd="apt install -y tk-dev curl make cmake xz-utils uuid-dev libdb-dev libssl-dev zlib1g-dev libbz2-dev libffi-dev libgdbm-dev liblzma-dev libsqlite3-dev build-essential libreadline-dev libncursesw5-dev"
+
+# sudoが使用可能かどうかをチェック
+if sudo -v > /dev/null 2>&1; then
+    echo "sudo is available"
+    sudo -E apt update
+    sudo -E $install_cmd
+else
+    echo "sudo is not available"
+    apt update
+    $install_cmd
+fi
 
 curl -O https://www.python.org/ftp/python/${PYTHON_VERSION}/Python-${PYTHON_VERSION}.tar.xz      
 tar xJf Python-$PYTHON_VERSION.tar.xz                                                        
